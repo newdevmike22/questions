998833570abfbe0749819c14c917d96a0709b517
MLDODSON <paola@dominicansoul.com>
2024-01-21 18:44:41 -0600

    Branch 2
    Branch 2 - Rendering the list of questions.

--- questions-app/src/components/Questions.jsx
+++ questions-app/src/components/Questions.jsx
@@ -1,7 +1,16 @@
-const Questions = () => {
+import SingleQuestion from "./SingleQuestion";
+
+const Questions = ({ questions }) => {
     return (
-      <h1>Questions</h1>
-    )
-  }
+      <section className="w-[90vw] my-[5rem] mx-auto bg-white rounded-md p-[2.5rem] max-w-[800px] shadow-lg">
+        <h1 className="text-[3.052rem] text-center mb-[3rem] text-[#334155]">Questions</h1>
+        {questions.map((question) => {
+          return (
+            <SingleQuestion key={question.id} {...question} />
+          )
+        })}
+      </section>
+    );
+  };
   
   export default Questions;
--- questions-app/src/components/SingleQuestion.jsx
+++ questions-app/src/components/SingleQuestion.jsx
@@ -1,6 +1,15 @@
-const SingleQuestion = () => {
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const SingleQuestion = ({ title, info }) => {
+    const [showInfo, setShowInfo] = useState(false);
     return (
-      <h1>Single Question</h1>
+      <article className="py-[1rem] px-[1.5rem] mb-[1rem] rounded-md border-2 border-solid border-[#e2e8f0] shadow-lg">
+        <header className="flex justify-between items-center gap-4">
+            <h5 className="font-semibold leading-[1.5] text-[#334155] text-[1.25rem]">{title}</h5>
+        </header>
+        <p className="mt-4 leading-[2] text-[#64748b]">{info}</p>
+      </article>
     )
   }
   
