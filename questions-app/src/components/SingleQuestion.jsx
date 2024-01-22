770a3e102a598c9ded2c656d7482ac3d71753259
MLDODSON <paola@dominicansoul.com>
2024-01-21 19:00:51 -0600

    Branch 3
    Branch 3 - Toggling the questions.

--- questions-app/src/components/SingleQuestion.jsx
+++ questions-app/src/components/SingleQuestion.jsx
@@ -7,8 +7,14 @@ const SingleQuestion = ({ title, info }) => {
       <article className="py-[1rem] px-[1.5rem] mb-[1rem] rounded-md border-2 border-solid border-[#e2e8f0] shadow-lg">
         <header className="flex justify-between items-center gap-4">
             <h5 className="font-semibold leading-[1.5] text-[#334155] text-[1.25rem]">{title}</h5>
+            <button 
+                className="question-btn" 
+                onClick={() => setShowInfo(!showInfo)}
+            >
+                {showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+            </button>
         </header>
-        <p className="mt-4 leading-[2] text-[#64748b]">{info}</p>
+        {showInfo && <p className="mt-4 leading-[2] text-[#64748b]">{info}</p>}
       </article>
     )
   }
