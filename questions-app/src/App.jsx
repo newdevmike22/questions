23899ad98ecd308c6d762a0e31bf2adf7ab82756
MLDODSON <paola@dominicansoul.com>
2024-01-22 09:47:37 -0600

    Branch 4
    Branch 4 - App's toggle refactor so that only 1 answer to a question appears one at a time.

--- questions-app/src/App.jsx
+++ questions-app/src/App.jsx
@@ -4,10 +4,20 @@ import Questions from "./components/Questions";
 
 const App = () => {
   const [questions, setQuestions] = useState(question);
+  const [activeID, setActiveID] = useState(null);
+
+  const toggleQuestion = (id) => {
+    const newActiveID = id === activeID ? null : id;
+    setActiveID(newActiveID);
+  };
 
   return (
     <main className="min-h-[100vh] flex justify-center items-center">
-      <Questions questions={questions} />
+      <Questions 
+        questions={questions} 
+        activeID={activeID} 
+        toggleQuestion={toggleQuestion}
+      />
     </main>
   )
 };
